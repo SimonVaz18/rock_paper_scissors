a038456d9bb4a512110269f07f25af15574bc311
svaz18 <svaz18@hotmail.com>
2025-06-08 19:18:50 +0100

    Add event listener and user/computer choice text output

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 const options = ["🪨", "📄", "✂️"];
+const buttons = document.querySelectorAll("options.button");
+const userChoiceDisplay = document.getElementById("user-choice");
+const computerChoiceDisplay = document.getElementById("computer-choice");
+const resultText = document.getElementById("result-text");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const userChoice = button.textContent;
+        const computerChoice = choices [Math.floor(random() * choices.length)];
+
+        userChoiceDisplay.textContent = `Your choice: ${userChoice}`;
+        computerChoiceDisplay.textContent = `Computer's choice: ${computerChocie}`;
+    })
+})
